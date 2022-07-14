007765e3b8f777d499d7472230cf1b84fd08874f
Nathan Hood <nathanjhood@googlemail.com>
2022-07-14 22:55:12 +0200

    Implemented Plugin Parameters

---
+++ Source/PluginParameters.h
@@ -0,0 +1,43 @@
+/*
+  ==============================================================================
+
+    PluginParameters.h
+    Created: 29 May 2022 7:58:00pm
+    Author:  Nathan J. Hood
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#ifndef PLUGINPARAMETERS_H_INCLUDED
+#define PLUGINPARAMETERS_H_INCLUDED
+
+#include <JuceHeader.h>
+
+class VicanekBiquadAudioProcessor;
+
+class Parameters
+{
+public:
+    //using APVTS = juce::AudioProcessorValueTreeState;
+    using Params = juce::AudioProcessorValueTreeState::ParameterLayout;
+    //==========================================================================
+    /** Constructor. */
+    Parameters(VicanekBiquadAudioProcessor& p);
+
+    //==========================================================================
+    /** Create Parameter Layout. */
+    static void setParameterLayout(Params& params);
+
+private:
+    //==========================================================================
+    // This reference is provided as a quick way for the wrapper to
+    // access the processor object that created it.
+    VicanekBiquadAudioProcessor& audioProcessor;
+    juce::AudioProcessorValueTreeState& state;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Parameters)
+};
+
+#endif //PLUGINPARAMETERS_H_INCLUDED
