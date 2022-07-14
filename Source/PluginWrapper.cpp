62c895ee006aa65608f2beb79cb4abc266b6bf78
Nathan Hood <nathanjhood@googlemail.com>
2022-07-14 22:54:21 +0200

    Implemented PluginWrapper

---
+++ Source/PluginWrapper.cpp
@@ -0,0 +1,88 @@
+/*
+  ==============================================================================
+
+    ProcessorWrapper.cpp
+    Created: 8 May 2022 9:38:17pm
+    Author:  StoneyDSP
+
+  ==============================================================================
+*/
+
+#include "PluginWrapper.h"
+#include "PluginProcessor.h"
+
+template <typename SampleType>
+ProcessWrapper<SampleType>::ProcessWrapper(VicanekBiquadAudioProcessor& p)
+    :
+    audioProcessor(p),
+    state(p.getAPVTS()),
+    setup(p.getSpec()),
+    frequencyPtr(dynamic_cast <juce::AudioParameterFloat*> (p.getAPVTS().getParameter("frequencyID"))),
+    resonancePtr(dynamic_cast <juce::AudioParameterFloat*> (p.getAPVTS().getParameter("bandwidthID"))),
+    gainPtr(dynamic_cast <juce::AudioParameterFloat*> (p.getAPVTS().getParameter("gainID"))),
+    transformPtr(dynamic_cast <juce::AudioParameterChoice*> (p.getAPVTS().getParameter("transformID"))),
+    outputPtr(dynamic_cast <juce::AudioParameterFloat*> (p.getAPVTS().getParameter("outputID"))),
+    mixPtr(dynamic_cast <juce::AudioParameterFloat*> (p.getAPVTS().getParameter("mixID")))
+{
+    jassert(frequencyPtr != nullptr);
+    jassert(resonancePtr != nullptr);
+    jassert(gainPtr != nullptr);
+    jassert(transformPtr != nullptr);
+    jassert(outputPtr != nullptr);
+    jassert(mixPtr != nullptr);
+
+}
+
+template <typename SampleType>
+void ProcessWrapper<SampleType>::prepare(juce::dsp::ProcessSpec& spec)
+{
+    mixer.prepare(spec);
+    filter.prepare(spec);
+    output.prepare(spec);
+
+    reset();
+    update();
+};
+
+template <typename SampleType>
+void ProcessWrapper<SampleType>::reset()
+{
+    mixer.reset();
+    filter.reset();
+    output.reset();
+};
+
+template <typename SampleType>
+void ProcessWrapper<SampleType>::process(juce::AudioBuffer<SampleType>& buffer, juce::MidiBuffer& midiMessages)
+{
+    midiMessages.clear();
+
+    update();
+
+    juce::dsp::AudioBlock<SampleType> block(buffer);
+
+    mixer.pushDrySamples(block);
+
+    juce::dsp::ProcessContextReplacing<SampleType> context(block);
+
+    filter.process(context);
+    output.process(context);
+    mixer.mixWetSamples(block);
+};
+
+template <typename SampleType>
+void ProcessWrapper<SampleType>::update()
+{
+    mixer.setWetMixProportion(mixPtr->get() * 0.01f);
+
+    filter.setFrequency(frequencyPtr->get());
+    filter.setResonance(resonancePtr->get());
+    filter.setGain(gainPtr->get());
+
+    filter.setTransformType(static_cast<TransformationType>(transformPtr->getIndex()));
+    output.setGainDecibels(outputPtr->get());
+};
+
+//==============================================================================
+template class ProcessWrapper<float>;
+template class ProcessWrapper<double>;
---
+++ Source/PluginWrapper.h
@@ -0,0 +1,69 @@
+/*
+  ==============================================================================
+
+    PluginWrapper.h
+    Created: 8 May 2022 9:38:17pm
+    Author:  StoneyDSP
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#ifndef PLUGINWRAPPER_H_INCLUDED
+#define PLUGINWRAPPER_H_INCLUDED
+
+#include <JuceHeader.h>
+#include "Modules/MatchedBiquads.h"
+
+class VicanekBiquadAudioProcessor;
+
+template <typename SampleType>
+class ProcessWrapper
+{
+public:
+    //==============================================================================
+    /** Constructor. */
+    ProcessWrapper(VicanekBiquadAudioProcessor& p);
+
+    //==========================================================================
+    /** Initialises the processor. */
+    void prepare(juce::dsp::ProcessSpec& spec);
+
+    /** Resets the internal state variables of the processor. */
+    void reset();
+
+    //==========================================================================
+    void process(juce::AudioBuffer<SampleType>& buffer, juce::MidiBuffer& midiMessages);
+
+    //==========================================================================
+    /** Updates the internal state variables of the processor. */
+    void update();
+
+private:
+    //==========================================================================
+    // This reference is provided as a quick way for the wrapper to
+    // access the processor object that created it.
+    VicanekBiquadAudioProcessor& audioProcessor;
+    juce::AudioProcessorValueTreeState& state;
+    juce::dsp::ProcessSpec& setup;
+
+    //==============================================================================
+    /** Instantiate objects. */
+    juce::dsp::DryWetMixer<SampleType> mixer;
+    MatchedBiquad<SampleType> filter;
+    juce::dsp::Gain<SampleType> output;
+
+    //==========================================================================
+    /** Parameter pointers. */
+    juce::AudioParameterFloat* frequencyPtr{ nullptr };
+    juce::AudioParameterFloat* resonancePtr{ nullptr };
+    juce::AudioParameterFloat* gainPtr{ nullptr };
+    juce::AudioParameterChoice* transformPtr{ nullptr };
+    juce::AudioParameterFloat* outputPtr{ nullptr };
+    juce::AudioParameterFloat* mixPtr{ nullptr };
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ProcessWrapper)
+};
+
+#endif //  PLUGINWRAPPER_H_INCLUDED
