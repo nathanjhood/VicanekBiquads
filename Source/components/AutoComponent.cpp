b709de073ebe2f02145119ba453056cbc3279fcb
Nathan Hood <nathanjhood@googlemail.com>
2022-07-15 04:12:20 +0200

    Removed junk from Component

--- Source/components/AutoComponent.cpp
+++ Source/components/AutoComponent.cpp
@@ -199,11 +199,8 @@ void AutoComponent::resized()
     int x = 20;
     bool first = true;
 
-    auto width = getWidth();
-    auto height = getHeight();
     auto bounds = getBounds();
 
-    auto absCentreX = getWidth() / 3;
     auto absCentreY = getHeight() / 3;
 
     for (auto* s : sliders)
